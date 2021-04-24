c60187cb2c6bdc1e0b5b00b31dcf5935b06eadd9
Thomas Sipe <tsipe@Toms-MacBook-Pro.local>
2021-04-24 19:57:29 -0400

    completed jupyter notebook

---
+++ app.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+
+@app.route("/api/v1.0/stations")
+
+@app.route("/api/v1.0/tobs")
+
+@app.route("/api/v1.0/&lt;start&gt;<br/>")
+
+@app.route("/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>")
+
+
+
+
+
